1b824f26684b3442bfeae5f6b24719ccc5d780ce
Nancy <nancy@logisticinfotech.co.in>
2019-03-28 13:47:30 +0530

    initial commit

---
+++ Example/Pods/Target Support Files/SampleArithmetic/SampleArithmetic-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SampleArithmetic : NSObject
+@end
+@implementation PodsDummy_SampleArithmetic
+@end
---
+++ SampleArithmetic/Classes/ArithmeticService.swift
@@ -0,0 +1,18 @@
+//
+//  ArithmeticService.swift
+//  Pods
+//
+//  Created by Nancy on 28/03/19.
+//
+
+import Foundation
+
+public class ArithmeticService{
+    public func addition(number1: Int, number2: Int) -> Int{
+        return number1 + number2
+    }
+    
+    public func subtraction(number1: Int, number2: Int) -> Int{
+        return number1 - number2
+    }
+}
